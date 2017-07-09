a8e8338967e57fb195472d6b82199ddc42353246
HostenMarijn <marijn.hosten@gmail.com>
2017-07-09 10:50:14 +0200

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+	// the database url to connect
+	//url : 'mongodb://localhost:27017/skeletonsDB'
+	url : 'mongodb://marijnhosten:maho0294@ds153422.mlab.com:53422/skeletons'
+}
