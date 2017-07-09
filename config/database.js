e4a5898a636980a92cb1377329e1be157aeb7aa4
HostenMarijn <marijn.hosten@gmail.com>
2017-07-09 12:46:37 +0200

    change db

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 module.exports = {
 
 	// the database url to connect
-	//url : 'mongodb://localhost:27017/skeletonsDB'
-	url : 'mongodb://marijnhosten:maho0294@ds153422.mlab.com:53422/skeletons'
+	url : 'mongodb://marijnhosten:AHx3kBXNi2x)#zyOXw@ds153392.mlab.com:53392/heroku_l3cplhb4'
 }
